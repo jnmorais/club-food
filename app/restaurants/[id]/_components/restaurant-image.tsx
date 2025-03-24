fa7ea6b9ecfaedc8cc0ad92f6c496ae18378b12b
jean <jsmorais2002@gmail.com>
2025-03-24 15:06:30 -0300

    feat: ensure restaurant can be favorited through restaurant page

--- app/restaurants/[id]/_components/restaurant-image.tsx
+++ app/restaurants/[id]/_components/restaurant-image.tsx
@@ -1,23 +1,49 @@
 "use client";
+
 import { Button } from "@/app/_components/ui/button";
-import { Restaurant } from "@prisma/client";
+import { isRestaurantFavorited } from "@/app/_helpers/restaurant";
+import useToggleFavoriteRestaurant from "@/app/_hooks/use-toggle-favorite-restaurant";
+import { Restaurant, UserFavoriteRestaurant } from "@prisma/client";
 import { ChevronLeftIcon, HeartIcon } from "lucide-react";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+
 interface RestaurantImageProps {
-  restaurant: Pick<Restaurant, "name" | "imageUrl">;
+  restaurant: Pick<Restaurant, "id" | "name" | "imageUrl">;
+  userFavoriteRestaurants: UserFavoriteRestaurant[];
 }
-const RestaurantImage = ({ restaurant }: RestaurantImageProps) => {
+
+const RestaurantImage = ({
+  restaurant,
+  userFavoriteRestaurants,
+}: RestaurantImageProps) => {
+  const { data } = useSession();
+
   const router = useRouter();
+
+  const isFavorite = isRestaurantFavorited(
+    restaurant.id,
+    userFavoriteRestaurants,
+  );
+
+  const { handleFavoriteClick } = useToggleFavoriteRestaurant({
+    restaurantId: restaurant.id,
+    userId: data?.user.id,
+    restaurantIsFavorited: isFavorite,
+  });
+
   const handleBackClick = () => router.back();
+
   return (
-    <div className="relative h-[215px] w-full">
+    <div className="relative h-[250px] w-full">
       <Image
         src={restaurant.imageUrl}
         alt={restaurant.name}
         fill
         className="object-cover"
       />
+
       <Button
         className="absolute left-4 top-4 rounded-full bg-white text-foreground hover:text-white"
         size="icon"
@@ -25,11 +51,16 @@ const RestaurantImage = ({ restaurant }: RestaurantImageProps) => {
       >
         <ChevronLeftIcon />
       </Button>
-       <Button className="absolute top-4 right-4 bg-gray-700 rounded-full h-7 w-7"> 
-            <HeartIcon size={18} className="fill-white"/>
-        </Button>
-      
+
+      <Button
+        size="icon"
+        className={`absolute right-4 top-4 rounded-full bg-gray-700 ${isFavorite && "bg-primary hover:bg-gray-700"}`}
+        onClick={handleFavoriteClick}
+      >
+        <HeartIcon size={20} className="fill-white" />
+      </Button>
     </div>
   );
 };
+
 export default RestaurantImage;
--- app/restaurants/[id]/page.tsx
+++ app/restaurants/[id]/page.tsx
@@ -4,22 +4,32 @@ import RestaurantImage from "./_components/restaurant-image";
 import Image from "next/image";
 import { StarIcon } from "lucide-react";
 import DeliveryInfo from "@/app/_components/delivery-info";
-import ProductList from "@/app/_components/product-list";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/_lib/auth";
 import CartBanner from "../_components/cart-banner";
+import ProductList from "@/app/_components/product-list";
 
 interface RestaurantPageProps {
   params: {
     id: string;
   };
 }
+
 const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
   const restaurant = await db.restaurant.findUnique({
-    where: { id },
+    where: {
+      id,
+    },
     include: {
       categories: {
+        orderBy: {
+          createdAt: "desc",
+        },
         include: {
           products: {
-            where: { restaurantId: id },
+            where: {
+              restaurantId: id,
+            },
             include: {
               restaurant: {
                 select: {
@@ -46,10 +56,20 @@ const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
   if (!restaurant) {
     return notFound();
   }
+  const session = await getServerSession(authOptions);
+
+  const userFavoriteRestaurants = await db.userFavoriteRestaurant.findMany({
+    where: {
+      userId: session?.user.id,
+    },
+  });
 
   return (
     <div>
-      <RestaurantImage restaurant={restaurant} />
+      <RestaurantImage
+        restaurant={restaurant}
+        userFavoriteRestaurants={userFavoriteRestaurants}
+      />
 
       <div className="relative z-50 mt-[-1.5rem] flex items-center justify-between rounded-tl-3xl rounded-tr-3xl bg-white px-5 pt-5">
         {/* TITULO */}
@@ -64,6 +84,7 @@ const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
           </div>
           <h1 className="text-xl font-semibold">{restaurant.name}</h1>
         </div>
+
         <div className="flex items-center gap-[3px] rounded-full bg-foreground px-2 py-[2px] text-white">
           <StarIcon size={12} className="fill-yellow-400 text-yellow-400" />
           <span className="text-xs font-semibold">5.0</span>
@@ -80,19 +101,22 @@ const RestaurantPage = async ({ params: { id } }: RestaurantPageProps) => {
             key={category.id}
             className="min-w-[167px] rounded-lg bg-[#F4F4F4] text-center"
           >
-            <span className="text-xs text-muted-foreground"></span>
-            {category.name}
+            <span className="text-xs text-muted-foreground">
+              {category.name}
+            </span>
           </div>
         ))}
       </div>
 
-      {/* TODO: mostrar produtos mais pedidos quando implementar realização de pedido */}
       <div className="mt-6 space-y-4">
-        <h2 className="px-5 font-semibold">Mais pedidos</h2>
+        {/* TODO: mostrar produtos mais pedidos quando implementarmos realização de pedido */}
+        <h2 className="px-5 font-semibold">Mais Pedidos</h2>
         <ProductList products={restaurant.products} />
       </div>
+
       {restaurant.categories.map((category) => (
         <div className="mt-6 space-y-4" key={category.id}>
+          {/* TODO: mostrar produtos mais pedidos quando implementarmos realização de pedido */}
           <h2 className="px-5 font-semibold">{category.name}</h2>
           <ProductList products={category.products} />
         </div>
